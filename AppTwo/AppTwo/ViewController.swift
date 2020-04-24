a28d47d6d990584430d770503156b40dbf87768f
Joel (Noel) <noel@onthegosports.com.au>
2020-04-24 20:28:52 +1000

    Lesson 2 added

---
+++ AppTwo/AppTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by Noel Angelo Borneo on 24/4/20.
+//  Copyright © 2020 Noel Angelo Borneo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
